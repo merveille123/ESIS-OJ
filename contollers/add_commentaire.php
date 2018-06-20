0c2094df6196ad45c033de1598a63998bfc3b9c5
tkz1937 <tekazayaeliel@gmail.com>
2018-06-19 22:13:22 -0700

    twelth commit

--- contollers/add_commentaire.php
+++ contollers/add_commentaire.php
@@ -8,16 +8,16 @@ require_once('../models/structure/etudiant.class.php');
 require_once('../models/dao/etudiant.dao.php');
 require_once('../models/structure/commentaire.class.php');
 require_once('../models/dao/commentaire.dao.php');
-if(isset($_POST['id_publication'],$_POST['contenu'])){
+if(isset($_POST['idpublication'],$_POST['contenu'])){
     $matricule = $_SESSION['matricule'];
     $etudiant_dao = new EtudiantDAO();
     $etudiant  = $etudiant_dao->getEtudiant($matricule);
     $id_etudiant = $etudiant->getId();
-    $id_publication = htmlspecialchars($_POST['id_publication']);
+    $id_publication = htmlspecialchars($_POST['idpublication']);
     $contenu = htmlspecialchars($_POST['contenu']);
     $commentaire = new Commentaire(null,$id_etudiant,$id_publication,$contenu,null,0,0);
     $commentaire_dao = new CommentaireDAO();
     $commentaire_dao->ajouterCommentaire($commentaire);
 }
-header('Location: ../views/suite.php?id='.$id_publication);
+header('Location: ../views/suite.php?idpublication='.$id_publication);
 ?>
