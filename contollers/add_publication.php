f27658340d224c0a3975ae8ba2433e33d8ec85e4
tkz1937 <tekazayaeliel@gmail.com>
2018-09-04 21:32:32 -0700

    [UPDATE]

--- contollers/add_publication.php
+++ contollers/add_publication.php
@@ -10,9 +10,11 @@ if(isset($_POST['contenu'])){
     $matricule = $_SESSION['matricule'];
     $etudiant_dao = new EtudiantDAO();
     $etudiant  = $etudiant_dao->getEtudiant($matricule);
+
     $id_etudiant = $etudiant->getId();
     $contenu = htmlspecialchars($_POST['contenu']);
     $post  = new Publication(null,$id_etudiant,$contenu,null,0,0);
+    
     $publication_dao = new PublicationDAO();
     $publication_dao->nouvellePublication($post);
 }
